da3629ff736ad0e13ee9655fa5445f106c8ae8a2
dongyuzhen <2790977664@qq.com>
2026-07-03 15:25:13 +0000

    fixed

---
+++ build/mpp/CMakeFiles/mpp_module.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mpp_module.dir/mpp.c.o"
+  "CMakeFiles/mpp_module.dir/mpp.c.o.d"
+  "CMakeFiles/mpp_module.dir/mpp_encoder.cpp.o"
+  "CMakeFiles/mpp_module.dir/mpp_encoder.cpp.o.d"
+  "libmpp_module.a"
+  "libmpp_module.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/mpp_module.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/pool/CMakeFiles/pool_module.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pool_module.dir/thread_poll.cpp.o"
+  "CMakeFiles/pool_module.dir/thread_poll.cpp.o.d"
+  "libpool_module.a"
+  "libpool_module.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pool_module.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
